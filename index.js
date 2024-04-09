ffd1ed7ed8422b3585606f1a231720c616d8971c
fee444 <felipeberteli1312@gmail.com>
2024-04-09 20:43:51 -0300

    Projeto finalizado!

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let nick = "Felipe"
-let exp = 3000
+let exp = 10001
 let nivel = ""
 
+    if ( exp < 1000) {
+    nivel = "Ferro"
+} else if (exp > 1000 && exp <= 2000){
+    nivel = "Bronze"
+}       else if (exp > 2000 && exp <= 5000){
+            nivel = "Prata"
+}               else if (exp > 5000 && exp <= 7000){
+                    nivel = "Ouro"
+}                       else if (exp > 7000 && exp <= 8000){
+                            nivel = "Platina"
+}                               else if (exp > 8000 && exp <= 9000){
+                                    nivel = "Ascendente "
+}                                       else if (exp > 9000 && exp <= 10000){
+                                                nivel = "Imortal"
+}                                                   else if (exp >= 10001){
+                                                        nivel = "Radiante"
+}                                                   
+console.log("O Herói de nome " + nick + " está no nível de " + nivel)
 
